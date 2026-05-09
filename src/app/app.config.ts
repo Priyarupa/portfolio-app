d312237ef374466092f8536ff4f0beb7450b400b
Priyarupa <priyanitysri002@gmail.com>
2026-05-09 13:02:48 +0530

    #dist files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,13 @@ import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideAnimationsAsync(),
     providePrimeNG({
      theme: {
     preset: Lara,
