ae8fc6f112cbb5aa07ca2bc74e1b4c7d5e359fe1
Priyarupa <priyanitysri002@gmail.com>
2026-05-09 12:39:53 +0530

    #gitignore changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from './side-bar/side-bar';
 import { NavBar } from './nav-bar/nav-bar';
-import { About } from "./about/about";
-import { Resume } from './resume/resume';
-import { Contact } from './contact/contact';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBar, NavBar, About,Resume,Contact],
+  imports: [RouterOutlet, SideBar, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { About } from '../about/about';
-import { Resume } from '../resume/resume';
-import { Contact } from '../contact/contact';
 import { TabsModule } from 'primeng/tabs';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [CommonModule,RouterLink,RouterLinkActive,About,Resume,Contact,TabsModule],
+  imports: [CommonModule,RouterLink,RouterLinkActive,TabsModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
