7e50470a3003d1fe37ee08bd26959aff1ab0fc86
Priyarupa <priyanitysri002@gmail.com>
2026-05-09 10:44:41 +0530

    #small chanes like css and adding icons

--- src/app/education/education.ts
+++ src/app/education/education.ts
@@ -21,7 +21,7 @@ events: EventItem[];
 
 constructor() {
         this.events = [
-                    { status: 'SRKR Engineering College', date: 'Aug. 2019 – April 2023(Grade:8.7)', icon: 'pi-graduation-cap', color:'#ffdb70' },
+                    { status: 'SRKR Engineering College', date: 'August 2019 – April 2023(Grade:8.7)', icon: 'pi-graduation-cap', color:'#ffdb70' },
                     { status: 'Aditya Junior College', date: 'April 2017 – March 2019(Grade:10.00)', icon: 'pi-graduation-cap', color:'#ffdb70'},
                     { status: 'Vidhya Nidhi School', date: 'April 2016 – March 2017(Grade:9.7)', icon: 'pi-graduation-cap',color:'#ffdb70'},
                 ];
